1b1d0a2a301629d423ca09993314d66a286c9dc5
ahmedsalik786 <ahmedsalik786@gmail.com>
2023-09-21 12:14:37 +0530

    project done

--- src/components/Compare/SelectCoin/index.js
+++ src/components/Compare/SelectCoin/index.js
@@ -1,3 +1,63 @@
+// import React, { useEffect, useState } from "react";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import Select from "@material-ui/core/Select";
+// import { get100Coin } from "../../../functions/get100Coin";
+// import "./style.css";
+
+// function SelectCoin({ crypto1, crypto2, handleCoinChange }) {
+//   const [allCoins, setAllcoins] = useState([]);
+
+//   useEffect(() => {
+//     getData();
+//   }, []);
+
+//   async function getData() {
+//     const myCoins = await get100Coin();
+//     setAllcoins(myCoins);
+//   }
+//   // useEffect(() => {
+//   //   console.log("salik", allCoins);
+//   // }, [allCoins]);
+
+//   return (
+//     <div className="coins-flex">
+//       <p>Crypto 1</p>
+//       <Select
+//         value={crypto1}
+//         label={crypto1}
+//         onChange={(event) => handleCoinChange(event,false)}
+//       >
+//         {allCoins
+//           .filter((item) => item.id !== crypto2)
+//           .map((coin, i) => (
+//             <MenuItem key={i} value={coin.id}>
+//               {coin.name}
+//             </MenuItem>
+//           ))}
+//       </Select>
+
+//       <p>Crypto 2</p>
+//       <Select
+//         value={crypto2}
+//         label={crypto2}
+//         onChange={(event) => handleCoinChange(event, true)}
+//       >
+//         {allCoins
+//           .filter((item) => item.id !== crypto1)
+//           .map((coin, id) => (
+//             <MenuItem key={id} value={coin.id}>
+//               {coin.name}{" "}
+//             </MenuItem>
+//           ))}
+//       </Select>
+//     </div>
+//   );
+// }
+
+// export default SelectCoin;
+
+
+
 import React, { useEffect, useState } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
@@ -5,18 +65,26 @@ import { get100Coin } from "../../../functions/get100Coin";
 import "./style.css";
 
 function SelectCoin({ crypto1, crypto2, handleCoinChange }) {
-  const [allCoins, setAllcoins] = useState([]);
+  const [allCoins, setAllCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getData();
   }, []);
 
   async function getData() {
-    const myCoins = await get100Coin();
-    setAllcoins(myCoins);
+    try {
+      const myCoins = await get100Coin();
+      setAllCoins(myCoins);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching coin data:", error);
+      setLoading(false);
+    }
   }
+
   useEffect(() => {
-    console.log("salik", allCoins);
+    console.log("allCoins", allCoins);
   }, [allCoins]);
 
   return (
@@ -25,15 +93,19 @@ function SelectCoin({ crypto1, crypto2, handleCoinChange }) {
       <Select
         value={crypto1}
         label={crypto1}
-        onChange={(event) => handleCoinChange(event)}
+        onChange={(event) => handleCoinChange(event, false)}
       >
-        {allCoins
-          .filter((item) => item.id !== crypto2)
-          .map((coin, i) => (
-            <MenuItem key={i} value={coin.id}>
-              {coin.name}
-            </MenuItem>
-          ))}
+        {loading ? (
+          <MenuItem value="">Loading...</MenuItem>
+        ) : (
+          allCoins
+            .filter((item) => item.id !== crypto2)
+            .map((coin, i) => (
+              <MenuItem key={i} value={coin.id}>
+                {coin.name}
+              </MenuItem>
+            ))
+        )}
       </Select>
 
       <p>Crypto 2</p>
@@ -42,13 +114,17 @@ function SelectCoin({ crypto1, crypto2, handleCoinChange }) {
         label={crypto2}
         onChange={(event) => handleCoinChange(event, true)}
       >
-        {allCoins
-          .filter((item) => item.id !== crypto1)
-          .map((coin, id) => (
-            <MenuItem key={id} value={coin.id}>
-              {coin.name}{" "}
-            </MenuItem>
-          ))}
+        {loading ? (
+          <MenuItem value="">Loading...</MenuItem>
+        ) : (
+          allCoins
+            .filter((item) => item.id !== crypto1)
+            .map((coin, id) => (
+              <MenuItem key={id} value={coin.id}>
+                {coin.name}
+              </MenuItem>
+            ))
+        )}
       </Select>
     </div>
   );
